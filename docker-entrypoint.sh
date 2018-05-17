b0a57fcaf51a0539bfbf6bac2b806f323e412f48
Luca Milanesio <luca.milanesio@gmail.com>
2018-05-17 14:08:21 +0100

    Add import of Gerrit ES mappings
    Change-Id: Ic5bf5829d2472bb8924fd99c4cb830b8fc9a3341

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 /wait-for-elasticsearch
 
+echo "** Importing Elasticsearch templates from: "
+for file in `ls -v /*.es.template.json`;
+    do  echo "--> $file";
+        curl -X PUT -v -H 'Content-Type: application/json' \
+             -d @$file http://elasticsearch:9200/_template/$(basename $file .es.template.json)
+done;
+
 echo "** Creating Gerrit index"
 curl -XPUT "http://elasticsearch:9200/gerrit?pretty" -H 'Content-Type: application/json'
 
-echo "** Importing Kibana setting from: "
-for file in `ls -v /*.json`;
+echo "** Importing Kibana settings from: "
+for file in `ls -v /*.kibana.data.json`;
     do  echo "--> $file";
         /usr/lib/node_modules/elasticdump/bin/elasticdump \
         --output=http://elasticsearch:9200/.kibana \
@@ -13,3 +20,4 @@ for file in `ls -v /*.json`;
         --type=data \
         --headers '{"Content-Type": "application/json"}';
 done;
+
